bc15bcf30ea5419d8c1a35e1229e63b51ed7a56e
Jörg Rathlev <joerg.rathlev@s24.com>
2017-12-29 12:10:05 +0100

    Exclude healthcheck from access logging

---
+++ src/main/java/com/s24/geoip/web/TomcatLoggingConfiguration.java
@@ -0,0 +1,59 @@
+package com.s24.geoip.web;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.catalina.valves.AccessLogValve;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.GenericFilterBean;
+
+/**
+ * Disables Logging for Healthchecks
+ */
+@Configuration
+public class TomcatLoggingConfiguration {
+
+    private static final String MARKER = "ignore-access-log";
+
+    @Bean
+    public FilterRegistrationBean loggingExcludeFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new GenericFilterBean() {
+            @Override
+            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                    throws IOException, ServletException {
+                request.setAttribute(MARKER, Boolean.TRUE);
+            }
+        });
+
+        registration.addUrlPatterns("/0.0.0.1");
+        return registration;
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer servletContainerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                    TomcatEmbeddedServletContainerFactory tomcat = (TomcatEmbeddedServletContainerFactory) container;
+
+                    tomcat.getEngineValves().stream()
+                            .filter(valve -> (valve instanceof AccessLogValve))
+                            .findFirst()
+                            .ifPresent(valve -> ((AccessLogValve) valve).setConditionUnless(MARKER));
+                }
+            }
+        };
+    }
+}
